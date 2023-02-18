0be6c73dca29825aa617c25b4f83839ab5b954c2
Taj Noah (AlAgawani) <33578536+tajalagawani@users.noreply.github.com>
2023-02-18 15:11:48 +0100

    Add a project structure

---
+++ stractuemer.py
@@ -0,0 +1,29 @@
+
+
+Project Structure: 
+
+- project_name/
+    - src/
+        - api/
+            - controllers/
+            - models/
+            - routes/
+        - components/
+            - auth/
+            - reports/
+            - tasks/
+            - users/
+        - views/
+            - index.html
+        - index.js
+    - config/
+        - config.js
+    - public/
+        - assets/
+            - images/
+            - css/
+            - js/
+    - server/
+        - server.js
+    - package.json
+    - README.md
