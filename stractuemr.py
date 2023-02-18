23fe20e64545fde57fa456ad86c1142f0a4bde01
Taj Noah (AlAgawani) <33578536+tajalagawani@users.noreply.github.com>
2023-02-18 15:03:37 +0100

    Add a project structure

---
+++ stractuemr.py
@@ -0,0 +1,27 @@
+
+
+Project Structure:
+/
+  ├── flask_app/
+  |   ├── __init__.py
+  |   ├── app.py
+  |   ├── models.py
+  |   ├── routes.py
+  |   └── utils.py
+  ├── migrations/
+  |   └── *.py
+  ├── static/
+  |   ├── css/
+  |   |   └── *.css
+  |   ├── js/
+  |   |   └── *.js
+  |   └── images/
+  |       └── *.jpg, *.png
+  └── templates/
+      ├── layout.html
+      ├── login.html
+      ├── registration.html
+      ├── profile.html
+      ├── feed.html
+      ├── post.html
+      └── follow.html
